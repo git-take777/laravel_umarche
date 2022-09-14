e3aac6fdde0f4a72572aec6bb94069d7649a8c18
take <gid.coding.life@gmail.com>
2022-09-14 17:22:27 +0900

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,5 +3,6 @@
     ヘッダー1
   </x-slot>
   コンポーネント1
-  <x-tests.card title="タイトル" content="本文" message="$message" />
+  <x-tests.card title="タイトル1" content="本文1" message="$message" />
+  <x-tests.card title="タイトル2" content="本文2" message="$message" />
 </x-tests.app>
