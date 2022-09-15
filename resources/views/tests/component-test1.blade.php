3ad2b7b4c8b97d2d96965bf5ce1018b21d4b6b3b
take <gid.coding.life@gmail.com>
2022-09-15 11:35:32 +0900

    追加

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,6 @@
   コンポーネント1
   <x-tests.card title="タイトル1" content="本文1" message="$message" />
   <x-tests.card title="タイトル2" content="本文2" message="$message" />
+  <x-tests.card title="CSSを変更したい" class="bg-red-300" content="本文3" message="$message" />
+
 </x-tests.app>
