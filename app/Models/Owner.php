6e064b5431680263acdd0d14ea0cac0a252ac368
take <gid.coding.life@gmail.com>
2022-09-24 22:47:31 +0900

    Shop追加

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use APP\Models\Shop;
 
 class Owner extends Authenticatable
 {
@@ -33,4 +34,9 @@ class Owner extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function shop() 
+    {
+        return $this->hasOne(Shop::class);
+    }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'テスト',
+            'email' => 'test@test.com',
+            'password' => Hash::make('password123'),
+            'created_at' => '2021/09/21 11:11:11',
+        ]);
+    }
+}
