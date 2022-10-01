19d8b6ff1267a24db357f4b2d5ff5d4170671d4d
take <gid.coding.life@gmail.com>
2022-10-01 19:43:27 +0900

    コミット

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -3,18 +3,24 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('admin.dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
-                </div>
+              
+              <!-- Logo -->
+              <div class="shrink-0 flex items-center">
+                <div class="w-12">
+                  <a href="{{ route('admin.dashboard') }}">
+                      <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                  </a>
+                </div><!-- /.w-12 -->
+              </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('admin.owners.index')" :active="request()->routeIs('admin.owners.index')">
+                      オーナー一覧
+                  </x-nav-link>
                 </div>
             </div>
 
